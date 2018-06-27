4e58be0c07c200412d43e5ecdd9e113222fff66d
JhaSYKO <38800815+JhaSYKO@users.noreply.github.com>
2018-06-27 08:42:25 -1000

    update authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule, Toast } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-// Native plug-ins
-import { Push } from '@ionic-native/push';
 
 
 import { MyApp } from './app.component';
@@ -14,7 +12,10 @@ import { RecapCPage } from './../pages/recap-c/recap-c';
 import { MenuTPage } from './../pages/menu-t/menu-t';
 import { MenuCPage } from './../pages/menu-c/menu-c';
 import { ListeTPage } from './../pages/liste-t/liste-t';
+
 import { AuthenticationPage } from './../pages/authentication/authentication';
+import { AuthService } from '../providers/auth-service/auth-service';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
@@ -25,12 +26,11 @@ import { AuthenticationPage } from './../pages/authentication/authentication';
     ListeTPage,
     MenuCPage,
     MenuTPage,
-    RecapCPage,
-    Toast,
-    Push
+    RecapCPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -41,14 +41,13 @@ import { AuthenticationPage } from './../pages/authentication/authentication';
     ListeTPage,
     MenuCPage,
     MenuTPage,
-    RecapCPage,
-    Toast,
-    Push
+    RecapCPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
   ]
 })
 export class AppModule {}
